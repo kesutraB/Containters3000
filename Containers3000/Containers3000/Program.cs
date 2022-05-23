3eb6b666e3c83272210bbfdbc09a0411b1679382
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-05-23 22:51:24 +0200

    started adding containers onto a ship, memory allocation issues

--- Containers3000/Containers3000/Program.cs
+++ Containers3000/Containers3000/Program.cs
@@ -7,7 +7,7 @@ namespace Containers3000
 {
 	internal class Program
 	{
-		public const int NumberOfBoxes = 200;
+		public const int NumberOfBoxes = 500;
 		public static List<Box> Boxes = new List<Box>();
 		public static List<Container> Containers = new List<Container>();
 		public static List<Ship> Ships = new List<Ship>();
@@ -15,38 +15,38 @@ namespace Containers3000
 		static void Main(string[] args)
 		{
 			Box nullBox = null;
-			Container nullContainer = null;
+			//Container nullContainer = null;
 			AddingBoxesUntilFull(NumberOfBoxes, 0, nullBox);
-			int numberOfContainers = Containers.Count;
-			AddingContainersUntilFull(numberOfContainers, 0, nullContainer);
+			//int numberOfContainers = Containers.Count;
+			//AddingContainersUntilFull(numberOfContainers, 0, nullContainer);
 
 			PrintPortTable();
 
 		}
 
-		private static void AddingContainersUntilFull(int notStoragedContainers, int firstContainer, Container nullContainer)
-		{
-			Ship ship = Ship.CreateShip();
-			Ships.Add(ship);
+		//private static void AddingContainersUntilFull(int notStoragedContainers, int firstContainer, Container nullContainer)
+		//{
+		//	Ship ship = Ship.CreateShip();
+		//	Ships.Add(ship);
 
-			for (int i = 0; i < notStoragedContainers; i++)
-			{
-				var container = Containers[i];
-				Containers.Add(container);
+		//	for (int i = 0; i < notStoragedContainers; i++)
+		//	{
+		//		var container = Containers[i];
+		//		Containers.Add(container);
 
-				if (!ship.DoesSmallerStorageFitIntoBiggerStorage(container))
-				{
-					Container notFittingContainer = container;
-					Containers.Remove(notFittingContainer);
-					AddingContainersUntilFull(notStoragedContainers, i, notFittingContainer);
-					return;
-				}
+		//		if (!ship.DoesSmallerStorageFitIntoBiggerStorage(container))
+		//		{
+		//			Container notFittingContainer = container;
+		//			Containers.Remove(notFittingContainer);
+		//			AddingContainersUntilFull(notStoragedContainers, i, notFittingContainer);
+		//			return;
+		//		}
 
-				ship.AddSmallerStorageToBiggerStorage(container, ship.ContainersInside);
+		//		ship.AddSmallerStorageToBiggerStorage(container, ship.ContainersInside);
 
-				nullContainer = null;
-			}
-		}
+		//		nullContainer = null;
+		//	}
+		//}
 
 		private static void AddingBoxesUntilFull(int notStoragedBoxes, int firstBox, Box nullBox)
 		{
