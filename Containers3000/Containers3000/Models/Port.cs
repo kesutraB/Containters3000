a00819448426cc4fa325f8976a6cf1642f9b536d
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-05-25 13:11:31 +0200

    doufam ze mi adam pomuze

--- Containers3000/Containers3000/Models/Port.cs
+++ Containers3000/Containers3000/Models/Port.cs
@@ -6,7 +6,9 @@ namespace Containers3000.Models
 	public class Port
 	{
 		public int DockingSpots { get; set; }
+		public List<int> Distances { get; set; }
 		public List<Ship> DockedShips { get; set; }
+		public Dictionary<int, Ship> DockingSpot { get; set; }
 		public List<Container> ContainersInDocks { get; set; }
 		public ContainerLocation Location { get; set; } = ContainerLocation.None;
 		public DockOccupation Occupation { get; set; } = DockOccupation.None;
@@ -15,7 +17,19 @@ namespace Containers3000.Models
 		{
 			DockingSpots = dockingSpots;
 			DockedShips = new List<Ship>();
+			Distances = GetDistances();
+			DockingSpot = new Dictionary<int, Ship>();
 			ContainersInDocks = new List<Container>();
+		}
+
+		public List<int> GetDistances()
+		{
+			Distances = new List<int>(DockingSpots - 1);
+			for (int i = 0; i < DockingSpots - 1; i++)
+			{
+				Distances.Add(GeneratingHelper.GetRng(100, 451));
+			}
+			return Distances;
 		}
 
 	}
